1e10756b3c96a96f58170ae95bba4b26f3d6f813
magnus <magnus@cloudmonkey.org>
2012-04-23 17:05:30 +0200

    Fixed iptables package for lucid

--- environments/production/modules/fw/manifests/install.pp
+++ environments/production/modules/fw/manifests/install.pp
@@ -10,8 +10,6 @@ class fw::install {
         } else {
           package {$fw::packages:
             ensure   => present,
-            provider => 'dpkg',
-            source   => '/tmp/iptables-persistent_0.5.3ubuntu1_all.deb'
           }
         }
       }
