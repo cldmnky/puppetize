777a67c6f2833381342f8acfb55ce85116648d81
magnus <magnus@cloudmonkey.org>
2012-04-24 11:22:05 +0200

    Added params stub for puppet module

---
+++ environments/production/modules/puppet/manifests/params.pp
@@ -0,0 +1,30 @@
+# Class: puppet::params
+#
+#  Sets params used in the puppet module 
+#
+# Parameters:
+#
+# Actions:
+#
+#            Requires:
+#
+# Sample Usage:
+#
+class puppet::params {
+  $module  = "puppet"
+  case $::operatingsystem {
+    'centos', 'redhat': {
+      $augpkgs = ["augeas", "ruby-augeas"],
+      $puppetpkgs = ['puppet'],
+      $puppetmasterpks = ['puppet-server']
+    }
+    'ubuntu', 'debian': {
+      $augpkgs = ["augeas-tools", "libaugeas-ruby", "libaugeas0"],
+      $puppetpkgs = ['puppet-common', 'puppet'],
+      $puppetmasterpks = ['puppetmaster']
+    }
+    'default': {
+      fail('Unknown OS')
+    }
+  }
+}
